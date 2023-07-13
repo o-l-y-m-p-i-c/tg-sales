2c7482f016c0739f85ceea1d80ccd79d72267d5c
NikShamin <o-l-y-m-p-i-c@mail.ru>
2023-07-13 12:07:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
 import './cafe.css'
-import $ from "jquery";
 import { createContext } from 'react';
 
 
@@ -28,23 +27,23 @@ function App() {
 
 
 
-  const init = () => {
+  // const init = () => {
 
-    window.Telegram.WebView.onEvent('theme_changed', setThemeClass);
-    setThemeClass();
-    window.Telegram.WebApp.ready();
-    window.Telegram.WebApp.MainButton.setParams({
-      is_visible: !!IState.canPay,
-      text: 'VIEW ORDER',
-      color: '#31b545'
-    }).hideProgress();
-    window.Telegram.WebApp.BackButton.show();
-    window.Telegram.WebApp.BackButton.onClick(window.Telegram.WebApp.close())
-    window.Telegram.WebApp.MainButton.onClick(window.Telegram.WebApp.close())
-    // Telegram.WebApp.BackButton.hide();
-    // setIState({ ...IState, apiUrl: "\/cafe\/api", userId: 0, userHash: null, debug: true })
+  //   window.Telegram.WebView.onEvent('theme_changed', setThemeClass);
+  //   setThemeClass();
+  //   window.Telegram.WebApp.ready();
+  //   window.Telegram.WebApp.MainButton.setParams({
+  //     is_visible: !!IState.canPay,
+  //     text: 'VIEW ORDER',
+  //     color: '#31b545'
+  //   }).hideProgress();
+  //   window.Telegram.WebApp.BackButton.show();
+  //   window.Telegram.WebApp.BackButton.onClick(window.Telegram.WebApp.close())
+  //   window.Telegram.WebApp.MainButton.onClick(window.Telegram.WebApp.close())
+  //   // Telegram.WebApp.BackButton.hide();
+  //   // setIState({ ...IState, apiUrl: "\/cafe\/api", userId: 0, userHash: null, debug: true })
 
-  }
+  // }
 
 
   function setThemeClass() {
