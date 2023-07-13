346d43fc3548d018842ee163820c1ba0e4254c3b
NikShamin <o-l-y-m-p-i-c@mail.ru>
2023-07-13 12:26:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 
 
 function App() {
 
   console.log(window.Telegram.WebApp)
+
+
+
+  const init = () => {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.MainButton.setParams({
+      is_visible: !!IState.canPay,
+      text: 'VIEW ORDER',
+      color: '#31b545'
+    }).hideProgress();
+    window.Telegram.WebApp.BackButton.show();
+    window.Telegram.WebApp.BackButton.onClick(close)
+    window.Telegram.WebApp.MainButton.onClick(close)
+
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  function close() {
+    window.Telegram.WebApp.close()
+  }
 
   return (
     <>
