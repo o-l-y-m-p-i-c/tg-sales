45a98d6e6c573f3ba53330b5cbfba7dcce5e732f
NikShamin <o-l-y-m-p-i-c@mail.ru>
2023-07-13 12:00:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
 
+import React, { useState, useEffect } from 'react';
+import './cafe.css'
+import $ from "jquery";
+import { createContext } from 'react';
+
+
+const IContext = createContext({})
+
+
+
 function App() {
+  const [IState, setIState] = useState({
+    canPay: true,
+    modeOrder: true,
+    totalPrice: 29,
+    apiUrl: '',
+    userId: '',
+    userHash: '',
+    debug: false,
+    order_data: [],
+    comment: '',
+    price: 0
+  })
+  let [counter, setCounter] = useState({
+    number: 0
+  })
+
+
+  useEffect(() => {
+    init()
+  }, [])
+
+
+  const init = () => {
+
+    window.Telegram.WebView.onEvent('theme_changed', setThemeClass);
+    setThemeClass();
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.MainButton.setParams({
+      is_visible: !!IState.canPay,
+      text: 'VIEW ORDER',
+      color: '#31b545'
+    }).hideProgress();
+    window.Telegram.WebApp.BackButton.show();
+    window.Telegram.WebApp.BackButton.onClick(window.Telegram.WebApp.close())
+    window.Telegram.WebApp.MainButton.onClick(window.Telegram.WebApp.close())
+    // Telegram.WebApp.BackButton.hide();
+    // setIState({ ...IState, apiUrl: "\/cafe\/api", userId: 0, userHash: null, debug: true })
+
+  }
+
+
+  function setThemeClass() {
+    document.documentElement.className = window.Telegram.WebApp.colorScheme;
+  }
+
+  const handleClick = (e) => {
+    setCounter({ ...counter, number: ++counter.number })
+    // window.Telegram.WebApp.HapticFeedback.impactOccurred('light');
+  }
+
   return (
-    <div className="App">
-      Hi all
-    </div>
+    <IContext.Provider value={{ IState, setIState }}>
+      <div className="App">
+        Hi all
+        <p>
+          {counter.number}
+        </p>
+        <button onClick={handleClick}>
+          Test click
+        </button>
+      </div>
+
+    </IContext.Provider>
+
+
+
   );
 }
 
